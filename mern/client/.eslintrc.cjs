498e6e96ba14ea862c4a36f5fb48ae65e35dc5a7
chakirou <56918303+chakirouAlabani@users.noreply.github.com>
2026-09-08 08:43:24 -0400

    Limit Week 3 lint scope to relevant React source

--- mern/client/.eslintrc.cjs
+++ mern/client/.eslintrc.cjs
@@ -7,7 +7,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'cypress/**',
+    'cypress.config.js',
+  ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -15,6 +20,7 @@ module.exports = {
     eqeqeq: 'error',
     'no-var': 'error',
     'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
